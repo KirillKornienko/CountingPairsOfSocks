a737e13a5176d2f37ce989d572fd130b43154108
Кирилл Корниенко <kirea9638@gmail.com>
2018-06-13 01:56:21 +0300

    Добавьте файлы проекта.

---
+++ Counting Pairs Of Socks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Counting_Pairs_Of_Socks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = new string[] { "gray", "black", "red", "purple", "purple", "gray", "black", "green", "black" };
+            Console.WriteLine(NumberOfPairs(array));
+        }
+
+        static int NumberOfPairs(string[] array)
+        {
+            int num = 0;
+            array = array.OrderBy(x => x).ToArray();
+
+            for (int i = 0; i < array.Length;)
+            {
+                int last_index = Array.LastIndexOf(array, array[i]);
+                num += (last_index - i + 1) / 2;
+                i = ++last_index;
+            }
+
+            return num;
+        }
+
+    }
+}
